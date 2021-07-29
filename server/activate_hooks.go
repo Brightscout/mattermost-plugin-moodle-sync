87dbf698982bbb09d8015da8e872efcc66e8f981
Manoj <manoj@brightscout.com>
2021-07-29 22:43:47 +0530

    Added router, apis and bot creation logic Made onActivate hook and initBotUser function Changed setting key from WebhookSecret to Secret Made verifyHttpSecret function and testConnection API

---
+++ server/activate_hooks.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/pkg/errors"
+
+	"github.com/mattermost/mattermost-server/v5/model"
+)
+
+const (
+	BotUserName    = "moodle"
+	BotDisplayName = "Moodle Bot"
+	BotDescription = "A bot account created by the moodle sync plugin."
+)
+
+func (p *Plugin) OnActivate() error {
+	if err := p.initBotUser(); err != nil {
+		return err
+	}
+
+	if err := p.OnConfigurationChange(); err != nil {
+		return err
+	}
+
+	p.router = p.InitAPI()
+
+	return nil
+}
+
+func (p *Plugin) initBotUser() error {
+	botID, err := p.Helpers.EnsureBot(&model.Bot{
+		Username:    BotUserName,
+		DisplayName: BotDisplayName,
+		Description: BotDescription,
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to ensure bot")
+	}
+
+	p.botID = botID
+	return nil
+}
--- server/plugin.go
+++ server/plugin.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"sync"
 
+	"github.com/gorilla/mux"
 	"github.com/mattermost/mattermost-server/v5/plugin"
 )
 
@@ -18,11 +18,14 @@ type Plugin struct {
 	// configuration is the active plugin configuration. Consult getConfiguration and
 	// setConfiguration for usage.
 	configuration *configuration
+	router        *mux.Router
+	botID         string
 }
 
 // ServeHTTP demonstrates a plugin that handles HTTP requests by greeting the world.
 func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+	p.API.LogDebug("New plugin request:", "Host", r.Host, "RequestURI", r.RequestURI, "Method", r.Method)
+	p.router.ServeHTTP(w, r)
 }
 
 // See https://developers.mattermost.com/extend/plugins/server/reference/
