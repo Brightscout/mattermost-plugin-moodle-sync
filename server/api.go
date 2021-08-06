ec9710a28db62d95a624d18070e0a01fea73a4b7
Manoj <manoj@brightscout.com>
2021-08-06 15:01:48 +0530

    Added the createChannelInTeam API handler Made a constants file for the urls

--- server/api.go
+++ server/api.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"runtime/debug"
 
+	"github.com/Brightscout/x-mattermost-plugin-moodle-sync/server/constants"
 	"github.com/pkg/errors"
 
 	"github.com/gorilla/mux"
@@ -23,7 +24,8 @@ func (p *Plugin) InitAPI() *mux.Router {
 	s := r.PathPrefix("/api/v1").Subrouter()
 
 	// Add the custom plugin routes here
-	s.HandleFunc("/test", p.handleTest).Methods(http.MethodPost)
+	s.HandleFunc(constants.PathTest, p.handleTest).Methods(http.MethodPost)
+	s.HandleFunc(constants.CreateChannelInTeam, p.createChannelInTeam).Methods(http.MethodPost)
 
 	// 404 handler
 	r.Handle("{anything:.*}", http.NotFoundHandler())
@@ -38,6 +40,61 @@ func (p *Plugin) handleTest(w http.ResponseWriter, r *http.Request) {
 	}
 
 	returnStatusOK(w)
+}
+
+func (p *Plugin) createChannelInTeam(w http.ResponseWriter, r *http.Request) {
+	if status, err := verifyHTTPSecret(p.configuration.Secret, r.FormValue("secret")); err != nil {
+		p.API.LogError(fmt.Sprintf("Invalid Secret. Error: %v", err.Error()))
+		http.Error(w, err.Error(), status)
+		return
+	}
+
+	teamName := r.URL.Query().Get("team_name")
+	team, teamErr := p.API.GetTeamByName(teamName)
+	if teamErr != nil {
+		http.Error(w, fmt.Sprintf("Invalid team name. Error: %v", teamErr.Error()), http.StatusBadRequest)
+		return
+	}
+
+	channel := model.ChannelFromJson(r.Body)
+	if channel == nil {
+		p.API.LogDebug("Invalid request body")
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	if channel.Name == "" {
+		p.API.LogDebug("Error: name cannot be empty")
+		http.Error(w, "Error: name cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	channel.TeamId = team.Id
+	channel.Type = model.CHANNEL_PRIVATE
+	channel.CreatorId = p.botID
+	channel.DisplayName = channel.Name
+
+	createdChannel, err := p.API.CreateChannel(channel)
+	if err != nil {
+		p.API.LogDebug(fmt.Sprintf("Failed to create channel. Error: %v", err.Error()))
+		http.Error(w, fmt.Sprintf("Failed to create channel. Error: %v", err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	if _, err = p.API.CreateTeamMember(team.Id, p.botID); err != nil {
+		p.API.LogDebug(fmt.Sprintf("Failed to add bot to team. Error: %v", err.Error()))
+		http.Error(w, fmt.Sprintf("Failed to add bot to team. Error: %v", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	if _, err = p.API.AddChannelMember(createdChannel.Id, p.botID); err != nil {
+		p.API.LogDebug(fmt.Sprintf("Failed to add bot to channel. Error: %v", err.Error()))
+		http.Error(w, fmt.Sprintf("Failed to add bot to channel. Error: %v", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	_, _ = w.Write([]byte(createdChannel.ToJson()))
 }
 
 func returnStatusOK(w http.ResponseWriter) {
---
+++ server/constants/urls.go
@@ -0,0 +1,6 @@
+package constants
+
+const (
+	PathTest            = "/test"
+	CreateChannelInTeam = "/channels"
+)
